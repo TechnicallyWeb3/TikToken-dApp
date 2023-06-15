4a0e954bb09a90d09f79c8dc508b2b4ef38273ce
technicallyweb3 <mancinoweb3@gmail.com>
2023-06-14 20:03:57 -0600

    Added commented HTTPS code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 11133;
 
 app.get('/user', async (req, res) => {
   try {
@@ -32,3 +32,47 @@ app.get('/user', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+// HTTPS version
+// const express = require('express');
+// const axios = require('axios');
+// const https = require('https');
+// const fs = require('fs');
+
+// const app = express();
+// const port = 3000;
+
+// // SSL certificate and private key paths
+// const privateKeyPath = '/etc/letsencrypt/live/example.com/privkey.pem';
+// const certificatePath = '/etc/letsencrypt/live/example.com/cert.pem';
+// const caBundlePath = '/etc/letsencrypt/live/example.com/chain.pem';
+
+// // Read SSL certificate and private key files
+// const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+// const certificate = fs.readFileSync(certificatePath, 'utf8');
+// const caBundle = fs.readFileSync(caBundlePath, 'utf8');
+
+// // HTTPS server options
+// const httpsOptions = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: caBundle
+// };
+
+// app.get('/user', async (req, res) => {
+//   // Your existing code for handling the request
+//   // ...
+
+//   // Forward the response data to the client
+//   res.send(userId);
+// });
+
+// // Create the HTTPS server
+// const server = https.createServer(httpsOptions, app);
+
+// // Start the server
+// server.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
