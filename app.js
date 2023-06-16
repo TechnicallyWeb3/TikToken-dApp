f5c462ca3f72b3dba4b92c0053793bc901521ea4
technicallyweb3 <mancinoweb3@gmail.com>
2023-06-16 17:23:05 -0600

    fixed gc port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 11133;
+const port = 8080;
 
 app.get('/user', async (req, res) => {
   try {
